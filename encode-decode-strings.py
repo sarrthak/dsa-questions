059df167cdad5b02dc5086e9a71d72ec2366f12a
sarrthak <sarthak.trip99@gmail.com>
2025-08-22 17:08:58 -0400

    feat: added primitive approach

--- encode-decode-strings.py
+++ encode-decode-strings.py
@@ -1,16 +1,29 @@
 from typing import List
 
+# The below is a primitive implementation for the encode and decode question. 
+# Time Complexity - O(n)
+# The below implementation fails for test cases - ["a1", "b"], and the functions are not inverse of each other. 
 class Solution:
     def encode(self, strs: List[str]) -> str:
         op: str = ""
         for i in strs:
-            op += i + "."
+            op += i + str(len(i))
         return op
             
     def decode(self, s: str) -> List[str]:
-        return s.split(".")[:-1]
+        op: List[str] = []
+        buff: str = ""
+        for i in s:
+            if i.isdigit():
+                op.append(buff)
+                buff = ""
+                continue
+            else:
+                buff = buff + i
+        
+        return op
         
 
 sol = Solution()
-encodedString = sol.encode(["neet","code","love","you"])
+encodedString = sol.encode(["a1", "b"])
 print(sol.decode(encodedString))
